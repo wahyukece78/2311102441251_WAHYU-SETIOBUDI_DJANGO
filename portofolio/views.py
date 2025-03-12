f4465a0320795a0da560b22b747a3989504f4de5
Wahyu Setiobudi <pratamairfan461@gmail.com>
2025-03-12 21:14:59 +0800

    added views.py dan update urls.py

---
+++ portofolio/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def home(request):
+    template_name = "home.html"
+    context = {
+        'title' : 'my home',
+        'welcome' :'welcome my home',
+    }
+    return render(request, template_name, context)
+
+
+def about(request):
+    template_name = "about.html"
+    context = {
+        'title' : 'about me',
+        'welcome' :'ini page about me',
+    }
+    return render(request, template_name, context)
